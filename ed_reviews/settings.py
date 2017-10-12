b7f0b8a11d3f7a26daede9b5efc3f79bc173b21d
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-10-12 06:32:49 -0300

    adding Token Auth

--- ed_reviews/settings.py
+++ ed_reviews/settings.py
@@ -125,9 +125,11 @@ STATIC_URL = '/static/'
 # Rest Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
